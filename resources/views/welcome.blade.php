c5071c2ab3d7192db41c867b9d469418ad6bf8cd
JimVogelzangs2 <jimvogelzangs@gmail.com>
2025-09-16 09:32:18 +0200

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,10 @@
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Laravel project</title>
+</head>
+<body>
+    <h1>WELKOM BIJ MIJN LARAVEL project</h1>
+</body>
+</html>
